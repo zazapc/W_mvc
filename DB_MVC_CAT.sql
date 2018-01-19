498e61e0fba6c553c1aff60186bec701f94af93f
zazapc <zazazazapc@outlook.fr>
2018-01-19 10:48:31 -0500

    Create DB_MVC_CAT.sql

---
+++ DB_MVC_CAT.sql
@@ -0,0 +1,48 @@
+-- phpMyAdmin SQL Dump
+-- version 4.0.10deb1
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Jan 19, 2018 at 10:46 AM
+-- Server version: 5.5.58-0ubuntu0.14.04.1
+-- PHP Version: 5.5.9-1ubuntu4.22
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `DB_MVC_CAT`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `PRODUITS`
+--
+
+CREATE TABLE IF NOT EXISTS `PRODUITS` (
+  `ID_PRODUIT` int(11) NOT NULL AUTO_INCREMENT,
+  `NOM_PRODUIT` varchar(25) NOT NULL,
+  `PRIX` double NOT NULL,
+  PRIMARY KEY (`ID_PRODUIT`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
+
+--
+-- Dumping data for table `PRODUITS`
+--
+
+INSERT INTO `PRODUITS` (`ID_PRODUIT`, `NOM_PRODUIT`, `PRIX`) VALUES
+(1, 'HP', 6347),
+(2, 'COMPAQ', 9876),
+(3, 'TOSHIBA', 6345),
+(4, 'SIEMENSE', 8768);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
